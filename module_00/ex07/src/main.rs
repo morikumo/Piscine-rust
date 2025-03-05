8a88d33ad779b8c4f52c8deec706293fa324d99f
morikumo <morikumo456@gmail.com>
2025-03-05 15:18:30 +0100

    fix: user github fixed i hope

---
+++ module_00/ex07/src/main.rs
@@ -0,0 +1,24 @@
+fn strpcmp(query: &[u8], pattern: &[u8]) -> bool{
+ println("hello");   
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strpcmp() {
+        assert!(strpcmp(b"abc", b"abc"));
+        assert!(strpcmp(b"abcd", b"ab*"));
+        assert!(!strpcmp(b"cab", b"ab*"));
+        assert!(strpcmp(b"dcab", b"*ab"));
+        assert!(!strpcmp(b"abc", b"*ab"));
+        assert!(strpcmp(b"ab000cd", b"ab*cd"));
+        assert!(strpcmp(b"abcd", b"ab*cd"));
+        assert!(strpcmp(b"", b"****"));
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
